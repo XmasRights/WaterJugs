187ac8266c088bd1d885751bc296a0ead4f007a4
XmasRights <chris.fonseka@googlemail.com>
2016-12-11 22:34:41 +0000

    Initial Commit - how exciting

---
+++ WaterJugs/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  WaterJugs
+//
+//  Created by Christopher Fonseka on 11/12/2016.
+//  Copyright © 2016 ChristopherFonseka. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
