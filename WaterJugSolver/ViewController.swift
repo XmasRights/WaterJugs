588911b6d9f966c7349aaba4cdeafeea14993763
Christopher Fonseka <chris.fonseka@googlemail.com>
2017-04-18 22:14:55 +0100

    Out with the old, in with the new Xcode project

---
+++ WaterJugSolver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  WaterJugSolver
+//
+//  Created by Christopher Fonseka on 18/04/2017.
+//  Copyright © 2017 Christopher Fonseka. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
